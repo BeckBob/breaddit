f1d4f801e8d8bca997580bbb092ed18f96a0a6fe
BeckyBox <unsworthbecky@gmail.com>
2023-11-24 13:44:43 +0000

    deleting console log

--- models/topics.model.js
+++ models/topics.model.js
@@ -9,7 +9,6 @@ exports.checkTopicExists = (query) => {
 const {topic} = query
     return db.query(`SELECT * FROM topics WHERE slug = $1`, [topic])
 .then(( {rows }) => {
-    console.log(rows)
     if(!rows.length) {
         return Promise.reject({status: 404, msg: "Not Found"})
     }
